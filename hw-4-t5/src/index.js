948e2047e28860eda18feeb736f4c030e80c2f20
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-03 18:16:00 +0300

    done

--- hw-4-t5/src/index.js
+++ hw-4-t5/src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Car(props) {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const classes = ['card']
+
+  
+  if (props.car.marked) {
+    classes.push('marked')
+  }
+
+  return (
+    <div className={classes.join('  ')} onClick={props.onMark}>
+      <div className="card-img">
+        <img
+          src={props.car.img}
+          alt={props.car.name} />
+      </div>
+      <h3>{props.car.name}</h3>
+      <p>{props.car.price}</p>
+    </div>
+  )
+}
+
+class App extends React.Component {
+  state = {
+    cars: [
+      {marked: false, name: 'BMW M2 Coupe', price: 20000, img: 'https://ag-spots-2017.o.auroraobjects.eu/2017/07/10/other/2880-1800-crop-bmw-m2-coupe-f87-c872010072017173625_1.jpg'},
+      {marked: false, name: 'AUDI TT', price: 15000, img: 'https://a.d-cd.net/rMye3d1H2fUcgk7zQKclkPdKpOk-1920.jpg'},
+      {marked: false, name: 'Rolls Royce', price: 50000, img: 'http://otzyvy-avtovladelcev.ru/img/auto-body-image/5867/95556.jpg'},
+      {marked: false, name: 'Mercedes amg coupe', price: 18000, img: 'https://images.caricos.com/m/mercedes-benz/2018_mercedes-benz_e-class_coupe/images/2560x1440/2018_mercedes-benz_e-class_coupe_300_2560x1440.jpg'}
+    ]
+  }
+
+  handleMarked(name) {
+    const cars = this.state.cars.concat()
+
+    const car  =  cars.find(c => c.name ===  name)
+    car.marked  = !car.marked
+
+    this.setState({ cars })
+  }
+
+  renderCars() {
+    return this.state.cars.map(car => {
+      return (
+        <Car car={car}
+        key={car.name + Math.random()} 
+        onMark={this.handleMarked.bind(this, car.name)}
+        />
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="list">
+
+          { this.renderCars() }
+
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
